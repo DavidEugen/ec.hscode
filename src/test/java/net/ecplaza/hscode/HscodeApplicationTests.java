26f3300cd8d4c7ad8d35faeb1794a872ace555f0
DavidEugen-Mac <platonicojju@gmail.com>
2023-10-19 13:47:28 +0900

    Initial commit

---
+++ src/test/java/net/ecplaza/hscode/HscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ecplaza.hscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
